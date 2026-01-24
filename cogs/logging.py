621b9426a207b412109c18ebcf258b7e58542a95
likerofturtles <singhdivyam664@gmail.com>
2026-01-24 20:18:27 +0530

    Components V2, but in logging.py baby!!!

--- cogs/logging.py
+++ cogs/logging.py
@@ -4,10 +4,47 @@ from discord.ext import commands
 from utils.log import LoggingManager
 from utils.checks import slash_mod_check
 
-class DestructiveConfirmationView(discord.ui.View):
-    def __init__(self):
+class DestructiveConfirmationView(discord.ui.LayoutView):
+    def __init__(self, title_text: str, body_text: str, color: discord.Color):
         super().__init__(timeout=30)
         self.value = None
+        self.title_text = title_text
+        self.body_text = body_text
+        self.color = color
+        self.build_layout()
+
+    def build_layout(self):
+        self.clear_items()
+
+        container = discord.ui.Container(accent_color=self.color)
+
+        container.add_item(discord.ui.Section(content=f"## {self.title_text}"))
+
+        container.add_item(discord.ui.Separator())
+
+        container.add_item(discord.ui.Section(content=self.body_text))
+
+        container.add_item(discord.ui.Separator())
+
+        actions = discord.ui.ActionRow(
+            discord.ui.Button(label="Cancel", style=discord.ButtonStyle.grey, custom_id="cancel_btn"),
+            discord.ui.Button(label="Delete Permanently", style=discord.ButtonStyle.red, custom_id="confirm_btn")
+        )
+        container.add_item(actions)
+
+        self.add_item(container)
+
+    async def interaction_check(self, interaction: discord.Interaction) -> bool:
+        custom_id = interaction.data.get("custom_id")
+        if custom_id == "confirm_btn":
+            self.value = True
+        elif custom_id == "cancel_btn":
+            self.value = False
+
+        if self.value is not None:
+            self.stop()
+            return True
+        return False
 
     @discord.ui.button(label="Cancel", style=discord.ButtonStyle.grey)
     async def no_button(self, interaction: discord.Interaction, button: discord.ui.Button):
@@ -87,24 +124,31 @@ class Logging(commands.Cog):
         if not exists:
             return await interaction.response.send_message("Logging is already disabled in this server.", ephemeral=True)
 
-        view = DestructiveConfirmationView(self)
-        await interaction.response.send_message(embed=discord.Embed(title="Pending Confirmation",
-                                                                    description=f"Are you sure you want to:\n* Disable logging.\n* Delete the logging channel from the database permanently.",
-                                                                    view=view, colour=discord.Colour(0x000000)))
+        body_content = f"Are you sure you want to:\n* Disable logging\n* Delete the logging channel from the database permanently."
+        view = DestructiveConfirmationView("Pending Confirmation", body_content, discord.Color.from_rgb(0, 0, 0))
+        await interaction.response.send_message(view=view)
         await view.wait()
 
         if view.value is None:
-            await interaction.edit_original_response(embed=discord.Embed(title="Timed Out",
-                                                                         ddescription=f"~~Are you sure you want to:\n* Disable logging.\n* Delete the saved channel from the database permanently.~~",
-                                                                         colour=discord.Colour.red()))
+            view.title_text = "Timed Out"
+            view.body_text = f"~~{body_content}~~"
+            view.color = discord.Color.red()
+            view.build_layout()
+            await interaction.edit_original_response(view=view)
         if view.value is False:
-            await interaction.edit_original_response(embed=discord.Embed(title="Action Canceled",
-                                                                         ddescription=f"~~Are you sure you want to:\n* Disable logging.\n* Delete the saved channel from the database permanently.~~",
-                                                                         colour=discord.Colour.red()))
-        if view.value is True:
+            view.title_text = "Action Canceled"
+            view.body_text = f"~~{body_content}~~"
+            view.color = discord.Color.red()
+            view.build_layout()
+            await interaction.edit_original_response(view=view)
+
+        else:
             await self.manager.logging_delete(interaction.guild_id)
-            await interaction.edit_original_response(embed=discord.Embed(title="Action Confirmed",
-                                                                         ddescription=f"~~Are you sure you want to:\n* Disable logging.\n* Delete the saved channel from the database permanently.~~",
-                                                                         colour=discord.Colour.green()))
+            view.title_text = "Action Confirmed"
+            view.body_text = f"~~{body_content}~~"
+            view.color = discord.Color.green()
+            view.build_layout()
+            await interaction.edit_original_response(view=view)
+
 async def setup(bot):
     await bot.add_cog(Logging(bot))
